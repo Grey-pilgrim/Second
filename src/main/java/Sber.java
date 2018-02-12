e2c5d06c9f40725d2babda4b89f001235f850c56
Grey-pilgrim <Grey_pilgrim@mail.ru>
2018-02-12 23:42:18 +0600

    123

--- src/main/java/Sber.java
+++ src/main/java/Sber.java
@@ -9,33 +9,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 
-
-
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class Sber {
-    WebDriver driver;
-    String baseUrl;
+    private WebDriver driver;
+    private String baseUrl;
 
 
     @Before
     public void setUp() throws Exception {
-        System.setProperty("webdriver.gecko.driver", "drv/geckodriver.exe");
         System.setProperty("webdriver.chrome.driver", "drv/chromedriver.exe");
-        baseUrl = "https://www.sberbank.ru/ru/person";
         driver = new ChromeDriver();
+        baseUrl = "https://www.sberbank.ru/ru/person";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-        driver.get(baseUrl);
+        //driver.get(baseUrl);
 
 
     }
 
     @Test
-    public void testSberbank () {
-        driver.findElement(By.xpath("//li[@class='alt-menu-collapser alt-menu-collapser_branch alt-menu-collapser_opened alt-menu-mid__item']")).click();
-        driver.findElement(By.xpath("//li[@class='alt-menu-collapser alt-menu-collapser_branch alt-menu-collapser_closed alt-menu-collapser_delayed-hover alt-menu-mid__item']//div[@class='alt-menu-collapser__area alt-menu-collapser__area_cols_3']//div[@class='alt-menu-collapser__hidder']//div[@class='alt-menu-collapser__column']//div[@class='alt-menu-list__item alt-menu-list__item_leaf alt-menu-list__item_closed alt-menu-list__item_level_1']//a[@class='kit-link kit-link_color_black alt-menu-list__link alt-menu-list__link_level_1']")).click();
-        assertTrue(driver.findElement(By.xpath("//li[@class='alt-menu-collapser alt-menu-collapser_branch alt-menu-collapser_closed alt-menu-collapser_delayed-hover alt-menu-mid__item']//div[@class='alt-menu-collapser__area alt-menu-collapser__area_cols_3']//div[@class='alt-menu-collapser__hidder']//div[@class='alt-menu-collapser__column']//div[@class='alt-menu-list__item alt-menu-list__item_leaf alt-menu-list__item_closed alt-menu-list__item_level_1']//a[@class='kit-link kit-link_color_black alt-menu-list__link alt-menu-list__link_level_1']")).getText().contains("Страхование путешественников"));
+    public void testSberbank () throws Exception {
+        driver.get(baseUrl);
+        driver.findElement(By.xpath("//*[@id=\"main\"]/div/div/table/tbody/tr/td/div/div/div/div/div/div[1]/div[1]/div[4]/div/div/div/ul/li[5]")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div/table/tbody/tr/td/div/div/div/div/div/div[1]/div[1]/div[4]/div/div/div/ul/li[5]/div/div/div[2]/div/a")).click();
+        assertEquals("Страхование путешественников", driver.findElement(By.xpath("//*[@id=\"main\"]/div/div/div/div/div/div/div/div[2]/div[2]/div/div/div/div[1]/div/div/div/div")).getText());
         driver.findElement(By.xpath("//img[@src='/portalserver/content/atom/contentRepository/content/person/travel/banner-zashita-traveler.jpg?id=f6c836e1-5c5c-4367-b0d0-bbfb96be9c53']")).click();
         driver.findElement(By.xpath("//div[@class='b-form-box-title ng-binding'][contains(text(),'Минимальная')]")).click();
         driver.findElement(By.xpath("//span[@ng-click='save()']")).click();
@@ -68,12 +67,6 @@ public class Sber {
         if(checkBox3.isSelected()){
             checkBox3.click();
         }
-
-
-
-
-
-
         }
 
     public void fillField(By locator, String value) {
@@ -95,9 +88,10 @@ public class Sber {
     }
 
     @After
-    public void afterTest () {
+    public void afterTest () throws Exception {
         driver.quit();
 
     }
+
 
 }
